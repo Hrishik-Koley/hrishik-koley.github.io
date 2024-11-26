04cf614791b2571e316525a8bd313114c762517f
Hrishik-Koley <hrishikkoley16@gmail.com>
2024-11-26 20:25:41 +0530

    changes

--- assets/js/fade-in.js
+++
@@ -1,8 +0,0 @@
-// Add this in your JavaScript file (e.g., `assets/js/main.js`) or inline
-document.addEventListener("DOMContentLoaded", function () {
-    const fadeInElement = document.getElementById("fade-in-element");
-  
-    // Add the 'fade-in-visible' class to start the fade-in effect
-    fadeInElement.classList.add("fade-in-visible");
-  });
-  
